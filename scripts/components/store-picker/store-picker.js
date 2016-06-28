ab6433dc0934eeb60a53eee6e178427b3f94a6f5
Aleksey Lovchikov <aleksey.lovchikov@yandex.ru>
2016-06-28 14:45:20 +0000

    goToStore func

--- scripts/components/store-picker/store-picker.js
+++ scripts/components/store-picker/store-picker.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 
+import { browserHistory } from 'react-router';
+
 import h from '../../helpers';
 
 /* Store Picker */
 export default class StorePicker extends Component {
+	goToStore(e) {
+		e.preventDefault();
+		let storeID = this.refs.storeId.value;
+		browserHistory.push('/store/' + storeID);
+	}
+	
 	render() {
 		const { props } = this;
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={ this.goToStore.bind(this) }>
 				<h2>Please Enter A Store</h2>
 				<input type="text" ref="storeId" defaultValue={ h.getFunName() } required />
-				<input type="submit" value="Send" />
+				<input type="submit" value="Submit" />
 			</form>
 		);
 	}
